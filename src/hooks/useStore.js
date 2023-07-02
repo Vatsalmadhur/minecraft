c863a5e76c80e9da3de699224824f54e3726f897
Madhur <bhartimadhur21@gmail.com>
2023-07-02 22:17:56 +0530

    useStore hook done,last commit was broken code

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -1,27 +1,24 @@
-import {create} from 'zustand';
-import {nanoid} from 'nanoid'; 
-export const useStore=create((set)=>{
-    texture:'dirt';
-    cubes:[
-        {
-            key:nanoid(),
-            position:[1,1,1],
-            texture:'dirt'
-        }
-    ];
-    addCube:(x,y,z)=>{
-        set((prev)=>({
-            cubes:[...prev.cubes,{key:nanoid(),
-            positon:[x,y,z],
-        texture:prev.texture}]
-
-        })
-
-
-    };
-    removeCube:()=>{};
-    setTexture:()=>{};
-    saveWord:()=>{};
-    resetWorld:()=>{};
-
-})
+import { create } from "zustand";
+import { nanoid } from "nanoid";
+export const useStore = create((set) => ({
+  texture: "dirt",
+  cubes: [
+    {
+      key: nanoid(),
+      position: [1, 1, 1],
+      texture: "dirt",
+    },
+  ],
+  addCube: (x, y, z) => {
+    set((prev) => ({
+      cubes: [
+        ...prev.cubes,
+        { key: nanoid(), positon: [x, y, z], texture: prev.texture },
+      ],
+    }));
+  },
+  removeCube: () => {},
+  setTexture: () => {},
+  saveWord: () => {},
+  resetWorld: () => {},
+}));
